4ed9ceb66acfeac96899fcfc11f8f32e814083be
Çlirim <53019723+clirimhodaj@users.noreply.github.com>
2020-04-27 14:39:37 +0200

    Implementimi i funksionit "importo"
    Fajllat qe importohen ruhen ne direktoriumin keys me emrin qe e jep shfrytezuesi. Funksioni automatikisht e kupton çfare lloj i çelsit eshte  ( ne baze te numrit te elementeve te xml fajllit ).

--- Projekti-DS Faza e pare/rsa.java
+++ Projekti-DS Faza e pare/rsa.java
@@ -13,6 +13,121 @@ import java.io.PrintWriter;
 
 public class rsa {
 
+public static void importo(String kuimportohet,String ngaimportohet) throws SAXException, IOException, ParserConfigurationException {
+		
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		
+	if(new File("keys/"+kuimportohet+".xml").exists() || new File("keys/"+kuimportohet+".pub.xml").exists()){
+            System.out.println("Gabim: Celesi "+kuimportohet+" ekziston paraprakisht.");			
+	}
+		
+	else if(ngaimportohet.startsWith("http://")||ngaimportohet.startsWith("https://")) {
+		URL url; //referenca https://mkyong.com/java/how-to-get-url-content-in-java/
+
+		try {
+			// get URL content
+			url = new URL(ngaimportohet);
+			URLConnection conn = url.openConnection();
+
+			// open the stream and put it into BufferedReader
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String inputLine;
+
+				
+			File file = new File("keys/"+kuimportohet+".pub.xml");
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			//use FileWriter to write file
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			while ((inputLine = br.readLine()) != null) {
+				bw.write(inputLine);
+			}
+
+			bw.close();
+			br.close();
+
+			System.out.println("Celesi publik u ruajt ne fajllin 'keys/"+kuimportohet+".pub.xml'.");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
+	else if(!ngaimportohet.endsWith(".xml")) {
+			System.out.println("Gabim: Fajlli i dhene nuk eshte celes valid.");
+	}
+		
+	else {
+		File from = new File (ngaimportohet);
+		Document document = builder.parse(from);
+		Element rootElement = document.getDocumentElement();
+		if (rootElement.getChildNodes().getLength()>5) {
+				
+				
+			NodeList modulus = document.getElementsByTagName("Modulus");
+			NodeList exponent = document.getElementsByTagName("Exponent");
+			String permbajtjam = modulus.item(0).getTextContent();
+			String permbajtjae = exponent.item(0).getTextContent();
+			try {
+			    
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+			// root elements
+			Document doc = docBuilder.newDocument();
+			Element rootElement1 = doc.createElement("RSAKeyValue");
+			doc.appendChild(rootElement1);
+
+			// child elements
+			Element firstname = doc.createElement("Modulus");
+			firstname.appendChild(doc.createTextNode(permbajtjam));
+			rootElement1.appendChild(firstname);
+
+				
+			Element lastname = doc.createElement("Exponent");
+			lastname.appendChild(doc.createTextNode(permbajtjae));
+			rootElement1.appendChild(lastname);
+				
+			// Transform XML Document to String and write the content into xml file 
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("keys/"+kuimportohet+".pub.xml"));
+
+			transformer.transform(source, result);
+			   
+			}
+			catch (ParserConfigurationException pce) {
+				pce.printStackTrace();
+				} catch (TransformerException tfe) {
+				tfe.printStackTrace();
+				}
+				
+				
+			from.renameTo(new File("keys/"+kuimportohet+".xml"));
+			System.out.println("Celesi publik u ruajt ne fajllin 'keys/"+kuimportohet+".xml'.");
+			System.out.println("Celesi publik u ruajt ne fajllin 'keys/"+kuimportohet+".pub.xml'.");
+				
+		}
+		else  {
+			from.renameTo(new File("keys/"+kuimportohet+".pub.xml"));
+			System.out.println("Celesi publik u ruajt ne fajllin 'keys/"+kuimportohet+".pub.xml'.");
+		}
+	}
+		
+		
+}
+
 public static void write(StringBuilder builder, String tag, BigInteger bigInt) throws UnsupportedEncodingException {
     builder.append("\t<");
     builder.append(tag);
